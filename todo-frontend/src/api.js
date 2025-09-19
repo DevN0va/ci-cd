7dd90bfdf87d991b3baedcb621eeacddc2fb67ab
Sagar170899 <chavan.g.sagar@gmail.com>
2025-09-19 14:06:29 +0530

    Updated todo-frontend/src/api.js

--- todo-frontend/src/api.js
+++ todo-frontend/src/api.js
@@ -4,21 +4,21 @@ import axios from "axios";
 const API = import.meta.env.VITE_API_URL || "/api";
 
 export const getTodos = async () => {
-  const res = await axios.get(`${API}/todos`);
+  const res = await axios.get(`${API}/api/todos`);
   return res.data;
 };
 
 export const addTodo = async (todo) => {
-  const res = await axios.post(`${API}/todos`, todo);
+  const res = await axios.post(`${API}/api/todos`, todo);
   return res.data;
 };
 
 export const updateTodo = async (id, todo) => {
-  const res = await axios.put(`${API}/todos/${id}`, todo);
+  const res = await axios.put(`${API}/api/todos/${id}`, todo);
   return res.data;
 };
 
 export const deleteTodo = async (id) => {
-  const res = await axios.delete(`${API}/todos/${id}`);
+  const res = await axios.delete(`${API}/api/todos/${id}`);
   return res.data;
 };
